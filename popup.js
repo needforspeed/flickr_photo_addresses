a62c1bbac479d369e2c01208357e8b3245ca6591
Yuncong Zhang <njitzyc@gmail.com>
2012-05-27 20:43:32 -0400

    change the logic to work with organize page

--- popup.js
+++ popup.js
@@ -16,15 +16,35 @@ function myFlickr()
   );
 }
 
-function getHTML()
-{
-    return document.body.outerHTML
-}
-
 myFlickr.prototype.ready = function ( o )
 {
   var self = this;
   
+  chrome.extension.onRequest.addListener
+  (
+    function(request, sender, sendResponse)
+    {
+      self.url = sender.tab.url;
+
+      if(sender.tab.url=='http://www.flickr.com/photos/organize/')
+      {
+        self.image_elems.orig = $(request.content).find("div.batch_photo img");
+      }
+      else
+      {
+        self.image_elems.orig = $(request.content).find('.rapidnofollow img.pc_img');
+      }
+
+      for (var i = 0; i < self.image_elems.orig.length; i++)
+      {
+        self.image_elems.s[i] = self.image_elems.orig[i].src;
+        self.image_elems.m[i] = self.image_elems.orig[i].src.replace(/(.+)\_s(\..+)/, "$1_z$2");
+        self.image_elems.l[i] = self.image_elems.orig[i].src.replace(/(.+)\_s(\..+)/, "$1_b$2");
+      }
+      self.show()
+    }
+  );
+
   $('input:radio').bind
   (
     'click', 
@@ -39,35 +59,23 @@ myFlickr.prototype.ready = function ( o )
     null,
     function(tab)
     {
-      self.url = tab.url;
-
-      if(self.url.substring(0, 21) != 'http://www.flickr.com')
+      if(tab.url.substring(0, 21) != 'http://www.flickr.com')
       {
-        alert("This extension only works on http://www.flickr.com domain");
+        self.show();
         return;
       }
-      else // Photo SET page
-      {
-        $.get
-        (
-          tab.url, 
-          function(data)
-          {
-            var page_str = data;
-            var page_dom = $(page_str)
-            self.image_elems.orig = page_dom.find('.rapidnofollow img.pc_img');
-            for (var i = 0; i < self.image_elems.orig.length; i++)
-            {
-              self.image_elems.s[i] = self.image_elems.orig[i].src;
-              self.image_elems.m[i] = self.image_elems.orig[i].src.replace(/(.+)\_s(\..+)/, "$1_z$2");
-              self.image_elems.l[i] = self.image_elems.orig[i].src.replace(/(.+)\_s(\..+)/, "$1_b$2");
-            }
-            self.show()
-          }
-        );
-      }
+
+      chrome.tabs.executeScript
+      (
+        tab.id, 
+        {
+          "code":"chrome.extension.sendRequest({\"content\":document.body.innerHTML})"
+        }
+      );
     }
   );
+
+
 }
 
 myFlickr.prototype.show = function ()
@@ -75,6 +83,14 @@ myFlickr.prototype.show = function ()
   var self = this;
   
   $('#processing').removeClass("hide");
+
+  if(!self.image_elems.orig.length)
+  {
+    $("#na").removeClass("hide");
+    $('#processing').addClass("hide");
+    $('#links_holder').addClass("hide");
+  }
+
   $('#links_holder').html('');
 
   self.img_size = $('input:checked').val();
